f9d71c9084db76105b69d8c8dc036a4f9f1f2261
Historn <arnaujiga@gmail.com>
2023-04-17 21:54:33 +0200

    config.xml NPCs by map

--- citm_desvj_project_template-L07/Game/Source/Scene.cpp
+++ citm_desvj_project_template-L07/Game/Source/Scene.cpp
@@ -582,7 +582,7 @@ void Scene::LoadNPC(SString mapName_)
 		npcItem->data->needToDestroy = true;
 	}
 	
-	for (pugi::xml_node itemNode = app->configNode.child("scene").child(mapName_.GetString()).child("npc"); itemNode; itemNode = itemNode.next_sibling("npc"))
+	for (pugi::xml_node itemNode = app->configNode.child("npcs").child(mapName_.GetString()).child("npc"); itemNode; itemNode = itemNode.next_sibling("npc"))
 	{
 		npc = (NPC*)app->entityManager->CreateEntity(EntityType::NPC);
 		npc->parameters = itemNode;
