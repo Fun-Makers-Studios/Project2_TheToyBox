8f05d3cef7daa9ac3c523e56412606da6294f1e0
AriSevcik <99729800+AriSevcik@users.noreply.github.com>
2023-04-17 11:15:50 +0200

    Editing map and MapSpriteSheet/Cleaning

--- TheToyBox_Template/Output/Assets/Maps/Temp_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Temp_tiles" tilewidth="32" tileheight="32" tilecount="6" columns="3">
- <image source="../Textures/Temp_tiles.png" width="96" height="64"/>
-</tileset>
