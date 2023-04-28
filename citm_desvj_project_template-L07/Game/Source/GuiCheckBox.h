1d07af66b08eec75d411f0250c9e289ef188bcd9
Adrià Pons Mensa <adria.pons.mensa@estudiantat.upc.edu>
2023-04-28 09:59:25 +0200

    Added Gui Slider + Checkbox files

---
+++ citm_desvj_project_template-L07/Game/Source/GuiCheckBox.h
@@ -0,0 +1,29 @@
+#ifndef __GUICHECKBOX_H__
+#define __GUICHECKBOX_H__
+
+#include "GuiControl.h"
+
+#include "Point.h"
+#include "SString.h"
+
+class GuiCheckBox : public GuiControl
+{
+public:
+
+	GuiCheckBox(uint32 id, SDL_Rect bounds, const char* text);
+	virtual ~GuiCheckBox();
+
+	bool Update(float dt);
+	bool Draw(Render* render);
+
+	int mouseX, mouseY;
+	unsigned int click;
+
+	bool canClick = true;
+	bool drawBasic = false;
+
+
+public:
+};
+
+#endif // __GUICHECKBOX_H__
---
+++ citm_desvj_project_template-L07/Game/Source/GuiSlider.h
@@ -0,0 +1,29 @@
+#ifndef __GUISLIDER_H__
+#define __GUISLIDER_H__
+
+#include "GuiControl.h"
+
+#include "Point.h"
+#include "SString.h"
+
+class GuiSlider : public GuiControl
+{
+public:
+
+	GuiSlider(uint32 id, SDL_Rect bounds, const char* text);
+	virtual ~GuiSlider();
+
+	bool Update(float dt);
+	bool Draw(Render* render);
+
+	int mouseX, mouseY;
+	unsigned int click;
+
+	bool canClick = true;
+	bool drawBasic = false;
+
+
+public:
+};
+
+#endif // __GUISLIDER_H__
